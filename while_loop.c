6c033d30cd19663603591883f7cf633208855c86
19raghavsingla-max <raghav26csu310@ncuindia.edu>
2026-09-22 16:56:16 +0530

    Track all project files

---
+++ while_loop.c
@@ -0,0 +1,20 @@
+// C program to using while loop to write 
+#include <stdio.h>
+int main()
+{
+    int count = 1;
+
+    while (count <= 10)
+    {
+        
+        printf("\n%d", count);
+
+        count++;
+    }
+    return 0;
+}
+
+
+
+
+
