ae621035a11e5ab2de909ca12f8dc7d7eaed3794
19raghavsingla-max <raghav26csu310@ncuindia.edu>
2026-09-22 08:34:40 +0530

    first.c

--- first.c
+++ first.c
@@ -1,9 +1,9 @@
-// this is my first c program
+// this is my first C program
 // my name is  raghav singla
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-     printf("\n\n\\n welcome\t\t \t\tto\n\n \n\nthe\t\t \t\tcoding\n\n \n\nworld\t\t");
-    //printf("The integer numbers is:%d \nthe float number is:%f", (123), (134.54));
+    printf("\n\n\\n welcome\t\t \t\tto\n\n \n\nthe\t\t \t\tcoding\n\n \n\nworld\t\t");
+    // printf("The integer numbers is:%d \nthe float number is:%f", (123), (134.54));
     return 0;
 }
