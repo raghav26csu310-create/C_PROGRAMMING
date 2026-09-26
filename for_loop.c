5873d2f30258d4537dbbeefd7aeba3984ef3bdfa
19raghavsingla-max <raghav26csu310@ncuindia.edu>
2026-09-26 20:32:05 +0530

    used for loop for creating c program

---
+++ for_loop.c
@@ -0,0 +1,89 @@
+// C program using for loop
+#include <stdio.h>
+int main()
+// {
+//     int i = 1024;
+//     for (; i; i >>= 1)
+//         printf("hello, world");
+//     return 0;
+// }
+
+
+
+
+// {
+//     int i;
+//     for (i = 0; i < 20; i++)
+//     {
+//         switch (i)
+//         {
+//         case 0:
+//             i += 5;
+//         case 1:
+//             i += 2;
+//         case 5:
+//             i += 5;
+//         default:
+//             i += 4;
+//         }
+//         printf("%d", i);
+//     }
+//     return 0;
+// }
+
+
+
+
+// {
+//     int i = -5;
+
+// while(i <= 5)
+// {
+//     if(i >= 0)
+//         break;
+//     else
+//     {
+//         i++;
+//         continue;
+//     }
+
+//     printf("Neso");
+// }
+// }
+
+
+
+
+// {
+//     int i = 0;
+
+//     for (printf("one\n"); i < 3 && printf(""); i++)
+//     {
+//         printf("Hi!\n");
+//     }
+// }
+// {
+//     unsigned int i = 500;
+
+//     while (i++ != 0);
+
+//     printf("%d", i);
+// }
+
+
+
+
+{
+    int x = 3;
+
+    if (x == 2);
+        
+    x = 0;
+
+    if (x == 3)
+        x++;
+    else
+        x += 2;
+
+    printf("X = %d", x);
+}
